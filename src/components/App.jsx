6786ee17c3db0acd3b564cc79af66cf913f608b4
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-04-22 15:13:41 -0700

    feature: add logo

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+// import logo from "../../logo.svg";
+
+import  {Main}  from "./main";
+
+//implemnet routes here
+
+function App() {
+  return (
+    <div >
+  <Main></Main>
+    </div>
+  );
+}
+
+export default App;

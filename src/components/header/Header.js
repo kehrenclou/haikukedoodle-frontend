50d295f1e3ba8fd4972dcb1716774eccc6f71bea
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-07-10 07:58:32 -0700

    feature: add onblur to mobile menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -56,6 +56,7 @@ export default function Header() {
                 : "button_type_mobile_closed"
             }`}
             onClick={handleMenuClick}
+            onBlur={handleMenuClick}
           ></button>
         ) : (
           ""

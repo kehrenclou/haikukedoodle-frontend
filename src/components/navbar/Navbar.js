52c11e7aeecf83c7dea96dc521577d1f96d4fd62
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-07-15 12:36:38 -0700

    refactor nav li elements for accessibility

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -22,63 +22,66 @@ export default function Navbar({ isLessThan600, onLinkClick }) {
   }
   return (
     <>
-      <ul className={isLessThan600 ? "nav nav_mobile" : "nav"} id="menu">
-        <div className="nav__container">
-          <li className="nav__item">
-            <NavLink
-              to="/"
-              className="nav__link"
-              style={({ isActive }) => {
-                return {
-                  color: isActive ? "#fdc9ef" : "",
-                  fontWeight: isActive ? "700" : "400",
-                };
-              }}
-              onClick={onLinkClick}
-            >
-              Home
-            </NavLink>
-          </li>
-
-          <li className="nav__item">
-            <NavLink
-              to="/about"
-              className="nav__link"
-              style={({ isActive }) => {
-                return {
-                  color: isActive ? "#fdc9ef" : "",
-                  fontWeight: isActive ? "700" : "400",
-                };
-              }}
-              onClick={onLinkClick}
-            >
-              About
-            </NavLink>
-          </li>
-
-          <li className="nav__item">
-            <NavLink
-              to="/faq"
-              className="nav__link"
-              style={({ isActive }) => {
-                return {
-                  color: isActive ? "#fdc9ef" : "",
-                  fontWeight: isActive ? "700" : "400",
-                };
-              }}
-              onClick={onLinkClick}
-            >
-              FAQ
-            </NavLink>
-          </li>
-          {isLoggedIn & !currentUser.isAnonymous ? (
-            <li className="nav__item">
-              <p className="nav__link nav__link_nolink">{currentUser.name}</p>
-            </li>
-          ) : null}
-        </div>
+      <ul
+        className={
+          isLessThan600 ? "nav nav_mobile nav__container" : "nav nav__container"
+        }
+        id="menu"
+      >
+        <li className="nav__item">
+          <NavLink
+            to="/"
+            className="nav__link"
+            style={({ isActive }) => {
+              return {
+                color: isActive ? "#fdc9ef" : "",
+                fontWeight: isActive ? "700" : "400",
+              };
+            }}
+            onClick={onLinkClick}
+          >
+            Home
+          </NavLink>
+        </li>
 
         <li className="nav__item">
+          <NavLink
+            to="/about"
+            className="nav__link"
+            style={({ isActive }) => {
+              return {
+                color: isActive ? "#fdc9ef" : "",
+                fontWeight: isActive ? "700" : "400",
+              };
+            }}
+            onClick={onLinkClick}
+          >
+            About
+          </NavLink>
+        </li>
+
+        <li className="nav__item">
+          <NavLink
+            to="/faq"
+            className="nav__link"
+            style={({ isActive }) => {
+              return {
+                color: isActive ? "#fdc9ef" : "",
+                fontWeight: isActive ? "700" : "400",
+              };
+            }}
+            onClick={onLinkClick}
+          >
+            FAQ
+          </NavLink>
+        </li>
+        {isLoggedIn & !currentUser.isAnonymous ? (
+          <li className="nav__item">
+            <p className="nav__link nav__link_nolink">{currentUser.name}</p>
+          </li>
+        ) : null}
+
+        <li className="nav__item nav__item_last">
           {isLoggedIn & !currentUser.isAnonymous ? (
             <button
               className="button button_type_logout"
